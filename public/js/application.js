b6528790f5c3e0f5c87d3c3e14ca1443c6c040b4
jkaihsu <jkai.hsu@gmail.com>
2013-05-02 17:43:47 -0700

    Added clear button

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,36 @@
 $(document).ready(function(){
 
+  $('#get_color').on('click', function(e){
+    e.preventDefault();
+    // var cellNum = Math.floor(Math.random() * 9)+1;
+    // var cellColor = (Math.random()*0xFFFFFF<<0).toString(16);
+    console.log('click on #get_color');
+
+    $.ajax ({
+      type: "POST",
+      url: "/color"
+    }).done(function(data){ 
+      
+      console.log(data);
+      $('#color_me li:nth-child(' + data.cell +')').css('background-color',  data.color );
+      console.log('i am here');
+
+    }).fail(function(){
+      console.log('not this time')
+    })
+
+  });
+
+  $('#clear_color').on('click', function(e){
+    console.log('click on #clear_color');
+
+    $('#color_me li').each(function(i, cell){
+      $(cell).css('background-color', 'white');
+      console.log(i);
+      console.log(cell);
+    })
+
+  })
+
+
 });
